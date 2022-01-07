950b4b27da41a7a11e1a3b21283a975738195f15
mnubi <rodger.kamau@student.moringaschool.com>
2022-01-07 10:28:13 +0300

    added admin only parameters

--- neighapp/admin.py
+++ neighapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib import admin
+
+from neighapp.models import Neighbourhood, Profile, Business, Contact, Post
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Contact)
+admin.site.register(Post)
