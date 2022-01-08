2bf0c1964c092cdb02c242e5de6bbc45b309a2b2
mnubi <rodger.kamau@student.moringaschool.com>
2022-01-08 15:12:01 +0300

    added forms for the user to use to collect data

---
+++ neighapp/form.py
@@ -0,0 +1,27 @@
+from .models import Business, Neighbourhood, Post, Profile
+from django.forms import ModelForm
+from django import forms
+
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user'] 
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_pic','email') 
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        fields=['name','email','description','neighbourhood']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields=['title','image','content','neighbourhood']
+
+  
